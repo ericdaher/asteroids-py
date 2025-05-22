626a5bfece1067705ee93ce576fcfc387ffc3773
ericdaher <ericdaher2@gmail.com>
2025-05-22 18:46:24 -0300

    created updatable and drawable groups

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -20,8 +24,9 @@ def main():
                 return
 
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
